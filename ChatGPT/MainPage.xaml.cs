5f9d0588f18715f20baaf502915551621464383b
diallosidy <diallosidymohamed99@gmail.com>
2024-05-23 19:26:27 +0200

    Ajoutez des fichiers projet.

---
+++ ChatGPT/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace ChatGPT
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSendClicked(object sender, EventArgs e)
+        {
+            reponse.Text = "...";
+            
+        }
+    }
+
+}
